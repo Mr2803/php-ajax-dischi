5b3d009e901b8d39bf83f9d10551449141d6952c
Mr2803 <rispoli.mar2803@gmail.com>
2019-12-11 14:23:51 +0100

    fix style

--- onlyPhp/index.php
+++ onlyPhp/index.php
@@ -30,9 +30,7 @@
         <div class="row">
             <div class="col-12 d-flex">
                 <div class="song-global d-flex">
-                    
-
-                        <?php printArr($database) ?>
+                    <?php printArr($database) ?>
                 </div>
             </div>
         </div>
